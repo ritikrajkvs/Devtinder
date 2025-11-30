312742ab71310cab1e76a01470d9dc1b97e796dc
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-12-01 02:47:53 +0530

    Update Profile.jsx

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -1,16 +1,13 @@
 import { useSelector } from "react-redux";
 import EditProfile from "./EditProfile";
 
-
 const Profile = () => {
   const user = useSelector((store) => store.user);
 
   return (
-    user && (
-      <div>
-        <EditProfile user={user} />
-      </div>
-    )
+    <div className="min-h-screen bg-[#0f172a] flex justify-center py-10 px-4 animate-fade-in">
+       {user && <EditProfile user={user} />}
+    </div>
   );
 };
 

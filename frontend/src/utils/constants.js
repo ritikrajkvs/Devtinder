7dcab9c5b65946f2cbe370160551b189362bb06f
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-11-30 22:31:22 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,9 @@
-// NOTE: Ensure this matches your Render URL exactly
-export const BASE_URL = "https://devtinder-8i1r.onrender.com/api";
+// frontend/src/utils/constants.js
+
+// Checks if you are running locally (localhost) or on the web
+const isLocal = window.location.hostname === "localhost";
+
+// Switch automatically so you don't have to edit this file constantly
+export const BASE_URL = isLocal 
+  ? "http://localhost:7777/api" 
+  : "https://devtinder-8i1r.onrender.com/api";

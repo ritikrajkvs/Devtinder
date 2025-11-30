00e8a74ecc913e666e32b3f641b30f3bf0b7a4aa
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-12-01 03:26:59 +0530

    Update UserCard.jsx

--- frontend/src/Components/UserCard.jsx
+++ frontend/src/Components/UserCard.jsx
@@ -126,7 +126,7 @@ const UserCard = ({ user }) => {
                         <div className="absolute inset-0 bg-gradient-to-r from-primary to-secondary opacity-100 transition-opacity group-hover/btn:opacity-90"></div>
                         <span className="relative z-10 flex items-center justify-center gap-2 font-black text-sm text-white tracking-wider">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 animate-pulse" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" /></svg>
-                            CONNECT
+                            Interested
                         </span>
                     </button>
                 </div>
